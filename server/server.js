cbd6198470084618d2efdb9956470aa41f52f4d5
andrewpiao1 <andrew.piao1@gmail.com>
2019-10-30 16:06:32 -0400

    set up env db and middleware

--- server/server.js
+++ server/server.js
@@ -1,7 +1,21 @@
 const express = require('express');
 
+//middleware
+const bodyParser = require('body-parser') //to send json data for requests
+const cookieParser = require('cookie-parser') //read cookies when we get requests
+
 //host the instance of express in "app"
 const app = express();
+const mongoose = require('mongoose')
+require('dotenv').config() //allows us to use the .env information here on server
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE)
+
+//register middleware
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 3002;
 
