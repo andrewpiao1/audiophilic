e4907d6cb8298fe59f777f2f7591103a22cc79ae
andrewpiao1 <andrew.piao1@gmail.com>
2019-10-30 16:25:21 -0400

    created user model and linked to express

---
+++ server/models/user.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.schema({
+  email:{
+    type: String,
+    required: true, //value is required every time
+    trim: true, //take out white spaces when stored
+    unique: 1
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  name: {
+      type: String,
+      required: true,
+      maxlength: 100, //to limit length of the str
+  },
+  lastname: {
+    type: String,
+    required: true,
+    maxlength: 100,
+  },
+
+  //below: not required
+  cart:{
+    type: Array,
+    default: [], //if there's nothing there, set to default
+  },
+  history:{
+    type: Array,
+    default: []
+  },
+  role:{ //when user registers, they need a role (admin rights)
+    type: Number,
+    default: 0
+  },
+  token:{
+    type: String
+  }
+
+
+
+});
+
+
+const User = mongoose.model(userSchema('User', )) //creat a model from with name 'User' using userSchema
+
+module.exports = { User }
--- server/server.js
+++ server/server.js
@@ -17,6 +17,17 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+//===============================
+//           USERS
+//===============================
+
+app.post('/api/users/register', (req, res)=>{
+  res.status(200);
+
+})
+
+
+
 const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, ()=>{
