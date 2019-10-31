f929b5f0ead7785e6caea8b38f1323b2f4ce91bd
andrewpiao1 <andrew.piao1@gmail.com>
2019-10-31 12:08:19 -0400

    server: (LOGIN) checks login pw with stored already hashed pw, generate token with that user, and save to cookies

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt-nodejs');
-const SALT_I = 10;
+const jwt = require('jsonwebtoken') //JSON Web Token defines a self-contained way for securely transmitting information between parties as a JSON object; this information can be verified/trusted b/c it is digitally signed (using a key); encryption
+
+require('dotenv').config(); //to be able to use "SUPERSECRETPASSWORD"
+
+const saltRounds = 10;
+
+
 
 const userSchema = mongoose.Schema({
-
   // === REQUIRED FIELDS ===
   email:{
     type: String,
@@ -26,8 +31,7 @@ const userSchema = mongoose.Schema({
     required: true,
     maxlength: 100,
   },
-
-  // === NOT 'REQUIRED' ===
+  // === NOT 'REQUIRED' FIELDS ===
   cart:{
     type: Array,
     default: [], //if there's nothing there, set to default
@@ -45,14 +49,19 @@ const userSchema = mongoose.Schema({
   }
 });
 
+const User = mongoose.model('User', userSchema) //creat a model from with name 'User' using userSchema
+
+
 //before we save() to db, we want to hash the password str (w/ bcrypt), using pre('[action]')
 
 userSchema.pre('save', function(next){   //next for when you're finished and want to complete the rest of the process
   let user = this; //ES5 requires we create an alias for 'this'
 
-  bcrypt.genSalt(SALT_I, (err, salt)=>{
+  bcrypt.genSalt(saltRounds, (err, salt)=>{
     if (err) return next(err);
 
+    // *** if user wants to change name for ex, we do not want to re-hash password
+    //     (we only want it when they want to change password)
     if(user.isModified('password')){  // if changing password ***
       bcrypt.hash(user.password, salt, null, (err, hash)=>{
         if (err) return next(err);
@@ -63,15 +72,34 @@ userSchema.pre('save', function(next){   //next for when you're finished and wan
     }else{ //move forward and not hash password again
       next();
     }
-
-
   })
 })
 
-//***  if user wants to change name for ex, we do not want to re-hash password (we only want it when they want to change password)
+//create a method in the Schema
+userSchema.methods.comparePassword = (candidatePassword, cb)=>{ //cb so when we are done w/ the fxn, we can trigger it
+  //since we are within the same fxn, 'this.password' will be the user.password
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch)=>{
+    if (err) return cb(err);
+
+    cb(null, isMatch) //the returned 'isMatch' is a boolean
+  })
+}
+
+userSchema.methods.generateToken = (cb)=>{
+  var user = this;
+
+  //based on:  user.id + password (of the environment, that only server knows)
+  var token = jwt.sign(user._id.toHexString(), process.env.SECRET)
+
+  user.token = token; //modify user, then save
+  user.save((err, user)=>{
+    if (err) return cb(err);
+
+    cb(null, user) //if everything is good, new user should have the same .password as .token
+  })
 
 
+}
 
-const User = mongoose.model('User', userSchema) //creat a model from with name 'User' using userSchema
 
 module.exports = { User }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const app = express(); //host the instance of express in "app"
 
 //middleware
 const bodyParser = require('body-parser') //to send json data for requests
 const cookieParser = require('cookie-parser') //read cookies when we get requests
+//register middleware
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-//host the instance of express in "app"
-const app = express();
+//database
 const mongoose = require('mongoose')
 require('dotenv').config() //allows us to use the .env information here on server
 
@@ -13,10 +17,10 @@ mongoose.Promise = global.Promise;
 mongoose.set('useCreateIndex', true) //so we can use 'unique' schema property
 mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
 
-//register middleware
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-app.use(cookieParser());
+let db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', ()=>{console.log('...connected to Mongoose!')});
+
 
 // Models
 const {User}  = require('./models/user')
@@ -28,15 +32,13 @@ const {User}  = require('./models/user')
 
 app.get('/', (req, res) => res.send('Hello world!'))
 
-
+//=== REGISTER new user ===
 app.post('/api/users/register', (req, res)=>{
 
   const user = new User(req.body)  //from the model
 
   user.save((err, doc)=>{
-    if(err) {
-      return res.json({success: false, err});
-    }
+    if(err) return res.json({success: false, err});
 
     res.status(200).json({
       success: true,
@@ -45,10 +47,42 @@ app.post('/api/users/register', (req, res)=>{
   })
 })
 
+//=== user LOGIN ===
+  // find the email (to see if user already in db)
+  // check password
+  // generate new token (if username and pass are correct)
+app.post('/api/users/login', (req,res)=>{
+
+  User.findOne({'email': req.body.email}, (err, user)=>{ //this 'user' is in reference to the json data we put in (not whole schema)
+    if (!user) return res.json({loginSuccess: false, message: 'Auth failed, email found'});
+
+    user.comparePassword(req.body.password, (err, isMatch)=>{
+      if( !isMatch ) return res.json({loginSuccess: false, message: 'Wrong password'})
+
+      //if 'isMatch' is true, generate token
+      user.generateToken((err, user)=>{
+        if (err) return res.status(400).send(err)
+
+        // if everything is okay, store token as a cookie
+        res.cookie('w_auth', user.token).status(200).json({ //([name of cookie], [value of cookie]) -> stores token as a cookie
+          loginSuccess: true
+        })
+
+      })
+
+    })
+
+
+  })
+
+
+
+
+})
 
 
 const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, ()=>{
-  console.log(`...Server running at ${PORT}!`)
+  console.log(`...SERVER running at ${PORT}!`)
 })
