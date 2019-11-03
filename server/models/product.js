515bdcf8d7c58f4594889e2197bb9858d9e7ab2b
andrewpiao1 <andrew.piao1@gmail.com>
2019-11-02 20:54:10 -0400

    created Products schema/model w/ objectId referencing for category models

---
+++ server/models/product.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = mongoose.Schema({
+  name: {
+    require: true,
+    type: String,
+    unique: 1,
+    maxlength: 100
+  },
+  description: {
+    require: true,
+    type: String,
+    maxlength: 10000
+  },
+  price:{
+    require: true,
+    type: Number,
+    maxlength: 255
+  },
+  shipping: {
+    required: true,
+    type: Boolean
+  },
+  available: {
+    required: true,
+    type: Boolean
+  },
+  power_output:{
+    required: true,
+    type: Number
+  },
+  sold:{
+    type: Number,
+    default: 0,    //setting default value when new product added
+    maxlength: 255
+  },
+  publish:{       //for new products in DB you don't want displayed yet
+    required: true,
+    type: Boolean
+  },
+  images:{
+    type: Array,
+    default: []
+  },
+
+  brand: { // making a reference to the _id of the 'brand' model
+    required: true,
+    type: Schema.Types.ObjectId, //telling it's a reference to an ObjectId (alias: mongoose.Schema.Types.ObjectId)
+    ref: 'Brand'
+  },
+  type: {
+    required: true,
+    type: Schema.Types.ObjectId,
+    ref: 'Type'
+  }
+
+}, {timestamps: true}); //to automatically generate a timestamp when product is added
+
+const Product = mongoose.model('Product', productSchema);
+
+
+module.exports = { Product }
